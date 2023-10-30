3df60dac4e883c7ae0cd7d723d97effd1070ab0c
med ali  ezz <mohamedali.ezzeddine@astrolab-agency.com>
2023-10-30 11:00:38 +0100

    add opendrawer

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.scss']
+  styleUrls: ['./content.component.scss'],
 })
 export class ContentComponent {
+  @Output() drawerEvent = new EventEmitter();
 
+  openCloseDrawer(): void {
+    this.drawerEvent.emit('opened');
+  }
 }
